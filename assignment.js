d4114990ccfdb3b5cf339d5e416ea05e9efadc44
satyatejaswini1234 <127006272+satyatejaswini1234@users.noreply.github.com>
2024-07-25 10:14:41 +0530

    COMMITqh20g9s5o

---
+++ assignment.js
@@ -0,0 +1,96 @@
+import './Assign.css';
+function Assign(){
+    return(
+        <div id="container">
+        <div id="in-container">
+            <center>
+                <img src="person.jpeg" alt="person" width="90px" height="90px"></img>
+                <h1>Scott Ayers<span id="inner">  .2nd</span></h1>
+                <p>Building Social Media Apps At Microsoft</p>
+                <br></br>
+               <img src="person.jpeg" alt="person" width="20px" height="20px"></img> 20 Mutual Connections
+               <br></br><br></br>
+               <button id="butt">Connect</button>
+            </center>
+        </div>
+         <div id="in-container">
+         <center>
+             <img src="person.jpeg" alt="person" width="90px" height="90px"></img>
+             <h1>Scott Ayers<span id="inner">  .2nd</span></h1>
+             <p>Building Social Media Apps At Microsoft</p>
+             <br></br>
+            <img src="person.jpeg" alt="person" width="20px" height="20px"></img> 20 Mutual Connections
+            <br></br><br></br>
+            <button id="butt">Connect</button>
+         </center>
+     </div>
+      <div id="in-container">
+      <center>
+          <img src="person.jpeg" alt="person" width="90px" height="90px"></img>
+          <h1>Scott Ayers<span id="inner">  .2nd</span></h1>
+          <p>Building Social Media Apps At Microsoft</p>
+          <br></br>
+         <img src="person.jpeg" alt="person" width="20px" height="20px"></img> 20 Mutual Connections
+         <br></br><br></br>
+         <button id="butt">Connect</button>
+      </center>
+  </div>
+   <div id="in-container">
+   <center>
+       <img src="person.jpeg" alt="person" width="90px" height="90px"></img>
+       <h1>Scott Ayers<span id="inner">  .2nd</span></h1>
+       <p>Building Social Media Apps At Microsoft</p>
+       <br></br>
+      <img src="person.jpeg" alt="person" width="20px" height="20px"></img> 20 Mutual Connections
+      <br></br><br></br>
+      <button id="butt">Connect</button>
+   </center>
+</div>
+ <div id="in-container">
+ <center>
+     <img src="person.jpeg" alt="person" width="90px" height="90px"></img>
+     <h1>Scott Ayers<span id="inner">  .2nd</span></h1>
+     <p>Building Social Media Apps At Microsoft</p>
+     <br></br>
+    <img src="person.jpeg" alt="person" width="20px" height="20px"></img> 20 Mutual Connections
+    <br></br><br></br>
+    <button id="butt">Connect</button>
+ </center>
+</div>
+ <div id="in-container">
+ <center>
+     <img src="person.jpeg" alt="person" width="90px" height="90px"></img>
+     <h1>Scott Ayers<span id="inner">  .2nd</span></h1>
+     <p>Building Social Media Apps At Microsoft</p>
+     <br></br>
+    <img src="person.jpeg" alt="person" width="20px" height="20px"></img> 20 Mutual Connections
+    <br></br><br></br>
+    <button id="butt">Connect</button>
+ </center>
+</div>
+ <div id="in-container">
+ <center>
+     <img src="person.jpeg" alt="person" width="90px" height="90px"></img>
+     <h1>Scott Ayers<span id="inner">  .2nd</span></h1>
+     <p>Building Social Media Apps At Microsoft</p>
+     <br></br>
+    <img src="person.jpeg" alt="person" width="20px" height="20px"></img> 20 Mutual Connections
+    <br></br><br></br>
+    <button id="butt">Connect</button>
+ </center>
+</div>
+ <div id="in-container">
+ <center>
+     <img src="person.jpeg" alt="person" width="90px" height="90px"></img>
+     <h1>Scott Ayers<span id="inner">  .2nd</span></h1>
+     <p>Building Social Media Apps At Microsoft</p>
+     <br></br>
+    <img src="person.jpeg" alt="person" width="20px" height="20px"></img> 20 Mutual Connections
+    <br></br><br></br>
+    <button id="butt">Connect</button>
+ </center>
+</div>
+</div> 
+    )
+}
+export default Assign;
